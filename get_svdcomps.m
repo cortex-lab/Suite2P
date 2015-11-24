ccd19778b4f749c1baa9214f7d134515abe1ff23
sfailor <samuel.failor@gmail.com>
2015-11-24 17:41:46 +0000

    mkdir bug fix
    Was checking for incorrect subdirectories and then creating them

--- get_svdcomps.m
+++ get_svdcomps.m
@@ -99,8 +99,8 @@ while 1
 end
 fclose(fid);
 
-if ~exist(fullfile(ops.ResultsSavePath, ops.mouse_name, ops.date), 'dir')
-    mkdir(fullfile(ops.ResultsSavePath, ops.mouse_name, ops.date));
+if ~exist(fullfile(ops.ResultsSavePath), 'dir')
+    mkdir(fullfile(ops.ResultsSavePath));
 end
 
 totF = [0 cumsum(ops.Nframes)];
