97d631101a4b61696c4fb1f7b3719ead19db5a80
marius10p <marius10p@gmail.com>
2016-01-15 16:56:48 +0000

    NEW MASTER_FILE
    Necessary for new tiff loading function. Reduces number of options in
    both master_file and db variable.

--- master_file.m
+++ master_file.m
@@ -9,53 +9,51 @@ else
 	error('toolbox_path does not exist, please change toolbox_path');
 end
 
-ops0.useGPU                 = 0; % if you can use a GPU in matlab this accelerate registration approx 3 times
+ops0.useGPU                 = 1; % if you can use a GPU in matlab this accelerate registration approx 3 times
 ops0.doRegistration         = 1;
-
 % root paths for files and temporary storage (ideally an SSD drive. my SSD is C)
-ops0.RootStorage            = '//zserver4/Data/2P';
-ops0.CopyDataLocally        = 1;
-ops0.TempStorage            = 'C:/DATA/'; % copy data locally first
-ops0.RegFileRoot            = 'C:/DATA/'; 
-ops0.ResultsSavePath        = 'D:/DATA/F';
-
-ops0.RegFileTiffLocation    = 'D:/DATA/'; % leave empty to NOT save registered tiffs
-ops0.nimgbegend             = 250; % how many frames to average at the beginning and end of each experiment
-
-ops0.DeleteBin              = 1; % set to 1 for batch processing on a limited hard drive
-ops0.DeleteRawOnline        = 1; % set to 1 for deleting local tiff files right after registration
-
-ops0.useImRead              = 1; % imread works faster from a local drive
-ops0.PhaseCorrelation       = 1; % set to 0 for non-whitened cross-correlation
-ops0.SubPixel               = Inf; % 2 is alignment by 0.5 pixel, Inf is the exact number from phase correlation
-% upsampling factor during registration, 1 for no upsampling is fastest, 2 gives
-% better subpixel accuracy
-ops0.registrationUpsample   = 1;
-ops0.showTargetRegistration = 1;
-ops0.NimgFirstRegistration  = 1000; 
-ops0.NiterPrealign          = 10;
+ops0.RegFileTiffLocation    = []; %'D:/DATA/'; % leave empty to NOT save registered tiffs
+ops0.RegFileRoot            = 'C:/DATA/tempreg';
 
 ops0.getROIs                = 1;
+ops0.getSVDcomps            = 0;
+ops0.nSVD                   = 1000; % how many SVD components to keep
+
+ops0.temp_tiff              = 'C:/DATA/temp.tiff'; % copy data locally first
+ops0.ResultsSavePath        = 'D:/DATA/F';
+ops0.PhaseCorrelation       = 1; % set to 0 for non-whitened cross-correlation
+ops0.SubPixel               = Inf; % 2 is alignment by 0.5 pixel, Inf is the exact number from phase correlation
+
+ops0.showTargetRegistration = 1;
+ops0.RootStorage            = '//zserver4.ioo.ucl.ac.uk/Data/2P';
 ops0.ShowCellMap            = 1;
+ops0.DeleteBin              = 1; % set to 1 to delete bin file after processing
+
+% these are modifiable settings for classifying ROIs post-clustering
+clustrules.MaxNpix                          = 500; % important
+clustrules.MinNpix                          = 30; % important
+clustrules.Compact                          = 2; % important
+clustrules.parent.minPixRelVar              = 1/10;
+clustrules.parent.PixelFractionThreshold    = 0.5; % 1/20;
+clustrules.parent.MaxRegions                = 10;
+
+% the following settings shouldn't need to be adjusted
+ops0.NavgFramesSVD          = 5000; % how many (pooled) frames to do the SVD based on
 ops0.Nk0                    = 1300;  % how many clusters to start with
 ops0.Nk                     = 650;  % how many clusters to end with
 ops0.nSVDforROI             = 1000;
-ops0.niterclustering        = 30;   % how many iterations of clustering
-ops0.sig                    = 0.5;  % spatial smoothing length for clustering; encourages localized clusters
+ops0.niterclustering        = 50;   % how many iterations of clustering
 
-ops0.getSVDcomps            = 0;
-ops0.NavgFramesSVD          = 5000; % how many (binned) timepoints to do the SVD based on
-ops0.nSVD                   = 1000; % how many SVD components to keep
-
-% these are modifiable settings for classifying ROIs post-clustering, these settings can be over-ridden in the GUI after running the pipeline
-clustrules.MaxNpix                          = 500; 
-clustrules.MinNpix                          = 30; 
-clustrules.Compact                          = 2; 
-clustrules.parent.minPixRelVar              = 1/10;
-clustrules.parent.PixelFractionThreshold    = 0.5; 
-clustrules.parent.MaxRegions                = 10;
+ops0.NimgFirstRegistration  = 500; 
+ops0.RegPrecision           = 'int16';
+ops0.RawPrecision           = 'int16';
+ops0.NiterPrealign          = 10;
 
 ops0.LoadRegMean   			= 0; % 
+ops0.nimgbegend             = 250; % how many frames to average at the beginning and end of each experiment
+
+% ops0.sig                    = 0;  % spatial smoothing constant
+% (encourages colocalized clusters) OBSOLETE
 
 %%
 for iexp = 1:length(db)        %3:length(db)          
