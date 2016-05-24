7e49e22cb111588b3b5e7eff08e3068d251fc81a
marius10p <marius10p@gmail.com>
2016-05-24 13:12:49 +0100

    added second option for extracting red channel

--- run_REDaddon.m
+++ run_REDaddon.m
@@ -2,6 +2,11 @@ function run_REDaddon(iexp, db, ops0)
 % red channel addon to already processed data
 
 ops = build_ops3(db(iexp), ops0);
-mimgR = red_channel_mean(ops);
+
+if sum(ops.expts==ops.expred)>0
+    mimgR = red_channel_mean(ops);
+else
+     mimgR = red_channel_mean2(ops);
+end
 
 add_red_channel(ops, mimgR);
