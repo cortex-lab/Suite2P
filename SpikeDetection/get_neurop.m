fda83d7ac7b6fe634cdf4bfc5c33a444ff6ddd2c
marius10p <marius10p@gmail.com>
2016-07-10 16:55:29 +0100

    new file for deconvolution

---
+++ SpikeDetection/get_neurop.m
@@ -0,0 +1,14 @@
+function [B err] = get_neurop(Ff, Fneu, F1, kerneli, npad)
+
+X = conv(kerneli, F1);
+
+X = X(npad + [1:numel(F1)]);
+
+A = cat(2, X, ones(numel(F1), 1), Fneu);
+
+AtA = (A'*A) / size(A,1) + 1e-6;
+FtA = (Ff'*A) / size(A,1);
+
+B = FtA / AtA;
+
+err = std(Ff - A * B');
