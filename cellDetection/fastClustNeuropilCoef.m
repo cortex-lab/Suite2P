a9e0614e5fe90371409254ae2e679bb5da9a656b
marius10p <marius10p@gmail.com>
2016-11-30 12:59:20 +0000

    cleaned up initializations in clustering algorithm

--- cellDetection/fastClustNeuropilCoef.m
+++ cellDetection/fastClustNeuropilCoef.m
@@ -4,7 +4,6 @@ U =  reshape(U, [], size(U,ndims(U)));
 
 U = bsxfun(@times, U, Sv'.^.5)';
 [nSVD, Npix] = size(U);
-% Fs = bsxfun(@rdivide, Fs, Sv.^.5);
 
 %% clustering options
 Ly = numel(ops.yrange);
@@ -14,30 +13,9 @@ Lx = numel(ops.xrange);
 ops.Nk0 = floor(sqrt(ops.Nk0))^2;
 
 Nk      = ops.Nk0;
-nsqrt = round(sqrt(Nk));
-
-xs = repmat(round(linspace(1, nsqrt, Lx)), Ly, 1);
-ys = repmat(round(linspace(1, nsqrt, Ly))', 1, Lx);
-iclust = xs + (ys-1) * nsqrt;
-
-clear xs ys
-
 niter   = ops.niterclustering;
 
-% xs = repmat(1:Lx, Ly, 1);
-% ys = repmat((1:Ly)', 1, Lx);
-%
-% randx = rand(1, Nk) * Lx;
-% randy = rand(1, Nk) * Ly;
-%
-% dx = repmat(xs(:), 1, Nk) - repmat(randx, numel(xs(:)), 1);
-% dy = repmat(ys(:), 1, Nk) - repmat(randy, numel(ys(:)), 1);
-%
-% dxy = dx.^2 + dy.^2;
-% [~, iclust] = min(dxy, [], 2);
 %%
-% clear dx dy
-
 if ops.ShowCellMap
     figure( 'Units', 'pixels', 'position', [100 100 900 900])
     colormap('hsv')
@@ -51,7 +29,6 @@ Sat = ones(Ly, Lx);
 err = zeros(niter,1);
 ops.meanV = gather(sum(Sv)/(Ly*Lx));
 
-Nk = ops.Nk0;
 Nkiter = round(linspace(ops.Nk0, ops.Nk, niter-2));
 Nkiter(end+1:(niter+1)) = ops.Nk;
 
@@ -60,44 +37,15 @@ Npix = Ly * Lx;
 M = .0001 * ones(1, Npix, 'single');
 
 ison = true(Nk,1);
-TileFactor = getOr(ops, {'TileFactor'}, 1); % this option can be overwritten by the user
-nTiles = ceil(TileFactor * (Ly+Lx)/2 / (10 * ops.diameter)); % neuropil is modelled as nTiles by nTiles
+S = getNeuropilBasis(ops, 'raisedcosyne');
 
-xc = linspace(1, Lx, nTiles);
-yc = linspace(1, Ly, nTiles);
-yc = yc';
-xs = 1:Lx;
-ys = 1:Ly;
-
-sigx = 4*(Lx - 1)/nTiles;
-sigy = 4*(Ly - 1)/nTiles;
-
-S = zeros(Ly, Lx, nTiles, nTiles, 'single');
-for kx = 1:nTiles
-    for ky = 1:nTiles
-        cosx = 1+cos(2*pi*(xs - xc(kx))/sigx);
-        cosy = 1+cos(2*pi*(ys - yc(ky))/sigy);
-        cosx(abs(xs-xc(kx))>sigx/2) = 0;
-        cosy(abs(ys-yc(ky))>sigy/2) = 0;
-        
-        S(:, :,ky, kx) = cosy' * cosx;
-    end
-end
-S = reshape(S, [], nTiles^2);
-S = normc(S);
-
-nBasis = nTiles^2 ;
+nBasis = size(S,2);
 PixL = ones(1, Lx * Ly)';
 %%
-% addpath('C:\CODE\Github\Suite2P\SpikeDetection')
-% f0      = 3; % sampling frequency
-% load('D:\CODE\MariusBox\SpikeDetection\kernel.mat')
-
-% Finv = (Fs*Fs')\Fs;
 
 Uneu = U;
-Ireg = diag([ones(Nk,1); zeros(nBasis,1)]);
-%
+% Ireg = diag([ones(Nk,1); zeros(nBasis,1)]);
+
 tic
 for k = 1:niter
     % recompute neuropil
@@ -115,6 +63,10 @@ for k = 1:niter
     Ucell = U - neuropil; %what's left over for cell model
     PixL = PixL';
     
+    if k==1
+        iclust = initialize_clusters(Ucell, Nk, 'random', Lx, Ly);       % 'Voronoi'
+    end
+    
     % recompute cell activities
     vs = zeros(nSVD, Nk); % initialize cell activities
     for i = 1:Nk
@@ -123,23 +75,15 @@ for k = 1:niter
             vs(:,i) = M(ix) * Ucell(:, ix)';
         end
     end
-    %
-    %     Ff = Fs' * vs;
-    %     vs = Finv * max(0, Ff);
-    %     [dcell, Ffr] = run_deconvolution2(Ff, f0, kernel);
-    %     vs = Finv * Ffr;
     
     vs = bsxfun(@rdivide, vs, sum(vs.^2,1).^.5 + 1e-8);% normalize activity vectors
     vs = single(vs);
     
     % recompute pixels' assignments
-   
     xs          = vs' * Ucell;
     
     [M, iclust] = max(xs,[],1);
-    %     Uneu        = U - bsxfun(@times, M, vs(:,iclust)); %what's left over for neuropil model
     Uneu        = U - bsxfun(@times, M, vs(:,iclust)); %what's left over for neuropil model
-    %     vs = double(vs);
     
     %     err(k)      = sum(sum((Uneu-neuropil).^2)).^.5;
     err(k)      = norm(Uneu(:)-neuropil(:));
@@ -184,9 +128,6 @@ for k = 1:niter
     
     
     if (rem(k,10)==1 || k==niter) && ops.ShowCellMap
-        %         imagesc(reshape(PixL, Ly, Lx), [0 2])
-        %         drawnow
-        %%
         lam = M;
         for i = 1:Nk
             ix = find(iclust==i);
