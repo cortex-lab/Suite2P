b7eaabe37eb8fdc7bdef0d5379f8ac3b3c3bbc9b
marius10p <marius10p@gmail.com>
2016-12-06 12:20:03 +0000

    redoing undone work

--- spikeDetection/deconvolution_standalone.m
+++ spikeDetection/deconvolution_standalone.m
@@ -14,16 +14,12 @@ ops.maxNeurop    = getOr(ops0, {'maxNeurop'}, Inf); % maximum allowed neuropil c
 ops.recomputeKernel = getOr(ops0, {'recomputeKernel'}, 1); % whether to estimate kernel from data
 
 % the kernel should depend on timescale of sensor and imaging rate
-mtau             = ops.imageRate * ops.sensorTau/ops.nplanes; 
+ops.fs           = getOr(ops, 'fs', ops.imageRate/ops.nplanes);
+mtau             = ops.fs * ops.sensorTau; 
 
 if nargin<3 || isempty(neu)
     neu = zeros(size(ca));
 end
-
-coefNeu = .8; % initialize neuropil subtraction coef with 0.8
-sd0     = std(ca-coefNeu*neu - my_conv2(ca-coefNeu*neu, 2, 1), [], 1);
-ca      = 2 * ca ./ repmat(1e-5 + sd0, size(ca,1), 1);
-neu     = 2 * neu ./ repmat(1e-5 + sd0, size(ca,1), 1);
 
 Params = [1 3 1 2e4]; %type of deconvolution, Th, Thi(nner loop), max Nspikes
 
@@ -32,11 +28,13 @@ kernel = exp([-1:ceil(5*mtau)]'/mtau);
 %
 npad        = 250;
 [NT, NN]    = size(ca);
+coefNeu = .8 * ones(1,NN); % initialize neuropil subtraction coef with 0.8
+
+caCorrected = ca - bsxfun(@times, neu, coefNeu);
 
 if ops.recomputeKernel
-    ops.fs                   = getOr(ops, 'fs', ops.imageRate/ops.nplanes);
     tlag                     = 1;
-    [kernel, mtau]           = estimateKernel(ops, ca - coefNeu * neu, tlag);
+    [kernel, mtau]           = estimateKernel(ops, caCorrected, tlag);
     
 %     [kernel, mtau, coefNeu]  = estimateKernel(ops, ca - coefNeu * neu, tlag);
     
@@ -47,49 +45,27 @@ kernel = normc(kernel(:));
 B = zeros(3, NN);
 
 kernelS   = repmat(kernel, 1, NN);
-err = zeros(NN, 1);
-F1 = zeros(size(ca), 'single');
 dcell = cell(NN,1);
 
 tic
-niter = 10;
-for iter = 1:niter
-%     plot(kernelS)
-%     drawnow
-    
-%     fprintf('%2.2f sec, iter %d... ', toc, iter)
-   
-    % determine neuropil and cell contributions
-    parfor icell = 1:size(ca,2)
-        [B(:,icell), err(icell)] = ...
-            get_neurop(ca(:,icell), neu(:,icell), F1(:, icell), kernelS(:,icell), npad);
-    end
-    
-    % determine and subtract the neuropil
-    F1 = ca - bsxfun(@times, neu, B(3,:)) - bsxfun(@times, ones(NT,1), B(2,:));
-    
-    % normalize signal 
-    sd   = std(F1 - my_conv2(F1, 2, 1), [], 1);
-    F1   = bsxfun(@rdivide, F1 , 1e-12 + sd/2);
 
-    % run the deconvolution to get fs etc\
-    if iter==niter
-        parfor icell = 1:size(ca,2)
-            [F1(:,icell),dcell{icell}] = ...
-                single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad,dcell{icell});
-        end
-    else
-        parfor icell = 1:size(ca,2)
-            [F1(:,icell)] = ...
-                single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad);
-        end
-    end
-%     disp([mean(F1(:)>1e-12)  mean(err(:))   ])
-    
-    F1   = bsxfun(@times, F1 , 1e-12 + sd/2);   
+
+% determine and subtract the neuropil
+F1 = caCorrected - bsxfun(@times, ones(NT,1), baselines);
+
+% normalize signal
+sd   = std(F1 - my_conv2(F1, 2, 1), [], 1);
+F1   = bsxfun(@rdivide, F1 , 1e-12 + sd/2);
+
+% run the deconvolution to get fs etc\
+parfor icell = 1:size(ca,2)
+    [sp(:,icell),dcell{icell}] = ...
+        single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad,dcell{icell});
 end
-%
-% fprintf('finished... \n')
+
+%     disp([mean(F1(:)>1e-12)  mean(err(:))   ])
+
+F1   = bsxfun(@times, F1 , 1e-12 + sd/2);
 
 % rescale baseline contribution
 for icell = 1:size(ca,2)
