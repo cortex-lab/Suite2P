32879a4d0aed50cc30e147ffec25e62cfcc85309
marius10p <marius10p@gmail.com>
2016-12-11 22:25:55 +0000

    running baseline correction before deconvolution, max skewness for neuropil subtraction

--- SpikeDetection/run_deconvolution3.m
+++ SpikeDetection/run_deconvolution3.m
@@ -43,12 +43,9 @@ end
 % the basis functions should depend on timescale of sensor and imaging rate
 mtau             = ops.imageRate * ops.sensorTau/ops.nplanes; 
 
-coefNeu = .9; % initialize neuropil subtraction coef with 0.8
-sd0     = std(Ff-coefNeu*Fneu - my_conv2(Ff-coefNeu*Fneu, 2, 1), [], 1);
-Ff      = 2 * Ff ./ repmat(1e-5 + sd0, size(Ff,1), 1);
-Fneu    = 2 * Fneu ./ repmat(1e-5 + sd0, size(Ff,1), 1);
+coefDefault = .9; % initialize neuropil subtraction coef with 0.8
 
-Params = [1 3 1 2e4]; %type of deconvolution, Th, Thi(nner loop), max Nspikes
+Params = [1 1 .5 2e4]; %type of deconvolution, Th, Thi(nner loop), max Nspikes
 
 % f0 = (mtau/2); % resample the initialization of the kernel to the right number of samples
 kernel = exp([-1:ceil(5*mtau)]'/mtau);
@@ -56,73 +53,41 @@ kernel = exp([-1:ceil(5*mtau)]'/mtau);
 npad = 250;
 [NT, NN] = size(Ff);
 
+ops.fs            = getOr(ops, 'fs', ops.imageRate/ops.nplanes);
+[coefNeu, inomax] = my_ica(Ff, Fneu, ops.fs, coefDefault);
+
+Ff = Ff - bsxfun(@times, Fneu, coefNeu(:)');
+
+% determine and subtract the running baseline
+[F1, ~] = estimate_baseline(Ff, ops);
+
 if ops.recomputeKernel
-    ops.fs = getOr(ops, 'fs', ops.imageRate/ops.nplanes);
-    tlag                     = 1;
-    [kernel, mtau]           = estimateKernel(ops, Ff - coefNeu * Fneu, tlag);
-    
-%     [kernel, mtau, coefNeu]  = estimateKernel(ops, Ff - coefNeu * Fneu, tlag);
-    
+    [kernel, mtau] = get1Dkernel(F1, ops.fs);
+
+%     [kernel, mtau]  = estimateKernel(ops, Ff, tlag);
+        
     fprintf('Timescale determined is %4.4f samples \n', mtau);
 end
 kernel = normc(kernel(:));
 %%
-B = zeros(3, NN);
-
 kernelS   = repmat(kernel, 1, NN);
-
-err = zeros(NN, 1);
-
-F1 = zeros(size(Ff), 'single');
-
 dcell = cell(NN,1);
 
-tic
-niter = 10;
-for iter = 1:niter
-%     plot(kernelS)
-%     drawnow
-    
-%     fprintf('%2.2f sec, iter %d... ', toc, iter)
-   
-    % determine neuropil and cell contributions
-    parfor icell = 1:size(Ff,2)
-        [B(:,icell), err(icell)] = ...
-            get_neurop(Ff(:,icell), Fneu(:,icell), F1(:, icell), kernelS(:,icell), npad);
-    end
-   
-    % determine and subtract the neuropil
-    F1 = Ff - bsxfun(@times, Fneu, B(3,:)) - bsxfun(@times, ones(NT,1), B(2,:));
-    
-    % normalize signal 
-    sd   = std(F1 - my_conv2(F1, 2, 1), [], 1);
-    F1   = bsxfun(@rdivide, F1 , 1e-12 + sd/2);
 
-    % run the deconvolution to get fs etc\
-    if iter==niter
-        parfor icell = 1:size(Ff,2)
-            [F1(:,icell),dcell{icell}] = ...
-                single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad,dcell{icell});
-        end
-    else
-        parfor icell = 1:size(Ff,2)
-            [F1(:,icell)] = ...
-                single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad);
-        end
-    end
-%     disp([mean(F1(:)>1e-12)  mean(err(:))   ])
-    
-    F1   = bsxfun(@times, F1 , 1e-12 + sd/2);   
+% run the deconvolution to get fs etc\
+parfor icell = 1:size(Ff,2)
+    [F1(:,icell),dcell{icell}] = ...
+        single_step_single_cell(F1(:,icell), Params, kernelS(:,icell), NT, npad,dcell{icell});
 end
-%
-% fprintf('finished... \n')
+
+mean(F1(:)>0)
 
 % rescale baseline contribution
 for icell = 1:size(Ff,2)
-    dcell{icell}.c    = dcell{icell}.c * sd(icell)/2;
-    dcell{icell}.B    = B(:,icell);
-    dcell{icell}.B(2) = dcell{icell}.B(2) * sd(icell);
+    dcell{icell}.c                      = dcell{icell}.c;    
+    dcell{icell}.neuropil_coefficient   = coefNeu(icell);
 end
+
 
 %%
 
--- spikeDetection/get1Dkernel.m
+++ spikeDetection/get1Dkernel.m
@@ -1,15 +1,18 @@
-function [kernel, tau] = get1Dkernel(caHigh, Mtau0, Tlag, fs)
+function [kernel, tau, Ball] = get1Dkernel(caHigh, fs)
 
-% caHigh = ca;
-[b1, a1] = butter(3, 2 * 1/50 * 1/fs, 'high');
-caHigh   = filtfilt(b1, a1, double(caHigh));
+Tlag = 1;
+
+caHigh = double(caHigh);
+% caHigh = bsxfun(@minus, caHigh, mean(caHigh,1));
+% [b1, a1] = butter(3, 2 * 1/300 * 1/fs, 'high');
+% caHigh   = filtfilt(b1, a1, caHigh);
 
 [NT NN] = size(caHigh);
 
 dataPred = caHigh(Tlag+1:NT,:);
 err = zeros(NT, NN);
 
-taus = exp(linspace(log(1), log(3*fs), 10));
+taus = exp(linspace(log(fs/10), log(2*fs), 20));
 
 dE = zeros(length(taus), 1);
 Ball = zeros(2, length(taus));
@@ -27,32 +30,23 @@ for j = 1:length(taus)
     Ypred = X*B;
     
     err(1:NT-Tlag, :)   = reshape(Ypred-Y, size(dataPred));
-    dErr                = filter(kernel, 1, flipud(B(1) * err));
-    
     Ball(:,j) = B;
-    dp(j) = mean(mean(dErr(2:NT-Tlag, :) .* cafilt(1:NT-Tlag-1, :)));
-    
-    
+        
     dE(j) = mean(err(:).^2);
 end
 
 [~, imin] = min(dE);
 p = exp(-1/taus(imin));
 %%
-tau2 = exp(linspace(log(0.5*fs), log(5*fs), 30));
-As = exp(-1./tau2);
 
-tau = fs * ones(1, numel(taus));
-for i  = 1:numel(taus)
-    ibest = find((As - exp(-1/taus(i))) .* As.^Tlag > Ball(1,i), 1);
-    if ~isempty(ibest)
-        tau(1,i) = -1./log(As(ibest));
-    end
-end
-disp(tau)
+As = exp(-1./taus) + Ball(1,:);
+tau = -1./log(As);
 
-% tau = tau(min(imin, numel(tau)));
+% disp(tau)
+
 tau = tau(imin);
 
 %%
 kernel  = exp(-[0:1:ceil(5*tau)]'/tau);
+
+% baselines = Ball(2,imin) / (1 - 1/(1-p) * Ball(1,imin));
