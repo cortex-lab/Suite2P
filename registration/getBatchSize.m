4de025e1e9e923a3e9b5530dd08fb3bfceaf4483
Marius Pachitariu <marius10p@gmail.com>
2017-01-16 19:04:57 +0000

    in case GPU field 'AvailableMemory' is missing

--- registration/getBatchSize.m
+++ registration/getBatchSize.m
@@ -1,7 +1,13 @@
 function batchSize = getBatchSize(nPixels)
 
 g = gpuDevice;
-batchSize = 2^(floor(log2(g.AvailableMemory))-6)/2^ceil(log2(nPixels));
+
+batchSize = 2^(floor(log2(8e9))-6)/2^ceil(log2(nPixels));
+if isfield(g, 'AvailableMemory')
+  batchSize = 2^(floor(log2(g.AvailableMemory))-6)/2^ceil(log2(nPixels));
+elseif isfield(g, 'FreeMemory')
+  batchSize = 2^(floor(log2(g.FreeMemory))-6)/2^ceil(log2(nPixels));
+end
 
 % The calculation was deducted from the following examples
 % batchSize = 2^25/2^ceil(log2(nPixels)); % works well on GTX 970 (which has 4 GB memory)
