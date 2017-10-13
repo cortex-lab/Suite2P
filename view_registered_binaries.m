0c4fd5e900fd4a4ea38d4d6056f3e4ac5b1b7708
carsen-stringer <carsen.stringer@gmail.com>
2017-10-13 15:55:01 +0100

    viewing binary files

---
+++ view_registered_binaries.m
@@ -0,0 +1,46 @@
+% function to load and view the registered binaries output from Suite2P
+
+% db file
+make_db_MP024;
+% which experiment to view
+iexp = 5;
+
+db = db(iexp);
+ops.ResultsSavePath = 'D:/DATA/F/';
+CharSubDirs = '';
+for k = 1:length(db.expts)
+    CharSubDirs    = [CharSubDirs num2str(db.expts(k)) '_'];
+end
+CharSubDirs = CharSubDirs(1:end-1);
+
+ops.ResultsSavePath = sprintf('%s//%s//%s//%s//', ops.ResultsSavePath, db.mouse_name, db.date, ...
+    CharSubDirs);
+regpath = sprintf('%s/regops_%s_%s.mat', ops.ResultsSavePath, db.mouse_name, db.date);
+load(regpath);
+
+%%
+iplane = 2; % which plane of the recording
+ops = ops1{iplane};
+fid  = fopen(ops.RegFile, 'r'); % opens the registered binary
+[Ly Lx] = size(ops.mimg1);    % size of binary in x,y
+
+clf;
+nt0 = 0;
+nimgbatch = 2000; % how many frames you can load at a time (will depend on RAM)
+while 1
+  data = fread(fid, Ly*Lx*nimgbatch, '*int16');
+  if isempty(data)
+    break;
+  end
+  data = reshape(data, Ly, Lx, []);
+  data = data(ops.yrange, ops.xrange, :);
+  NT   = size(data,3);
+  for j = 1:NT
+    imagesc(data(:,:,j));
+    title(sprintf('frame %d', j+nt0));
+    drawnow;
+    pause(0.1);
+  end
+  nt0 = nt0 + NT;
+end
+fclose all;
