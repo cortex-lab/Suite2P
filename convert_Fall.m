c27250f219f974ad68f70c3137e6d920be28cdfa
Carsen Stringer <carsen.stringer@gmail.com>
2018-12-01 10:56:03 -0500

    issue with flipping dimensions

--- convert_Fall.m
+++ convert_Fall.m
@@ -5,24 +5,28 @@
 dat = load('Fall.mat');
 
 %%
-Fcell = {dat.F};
-FcellNeu = {dat.Fneu};
+NN = 1000;
+ineu = randperm(size(dat.F,1),NN);
+Fcell = {dat.F(ineu,:)};
+FcellNeu = {dat.Fneu(ineu,:)};
 ops = dat.ops;
-ops.Ly = single(ops.Ly);
-ops.Lx = single(ops.Lx);
+ops.Ly = single(dat.ops.Ly);
+ops.Lx = single(dat.ops.Lx);
 
 ops.mimg1 = ops.meanImg;
 if isfield(ops, 'meanImg_chan2')
 	ops.mimgRED = ops.meanImg_chan2;
 end
 sp = {dat.spks};
-
+clear stat
 flds = fieldnames(dat.stat{1});
-for n = 1:length(dat.stat)
+n=0;
+for l = ineu%length(dat.stat)
+	n=n+1;
 	for j = 1:length(flds)
-		stat(n).(flds{j}) = dat.stat{n}.(flds{j});
+		stat(n).(flds{j}) = dat.stat{l}.(flds{j});
 	end
-	stat(n).ipix = int64(ops.Lx)*(stat(n).xpix) + stat(n).ypix + 1;
+	stat(n).ipix = int64(ops.Ly)*(stat(n).xpix) + stat(n).ypix + 1;
 	stat(n).ipix = stat(n).ipix(:);
 	stat(n).mimgProjAbs = 0;
 	stat(n).cmpct = stat(n).compact;
@@ -36,5 +40,9 @@ ops.xrange_crop = dat.ops.xrange(1)+1:dat.ops.xrange(end);
 
 ops.Vcorr = zeros(dat.ops.Ly, dat.ops.Lx);
 ops.Vcorr(ops.yrange_crop, ops.xrange_crop) = dat.ops.Vcorr;
+
+
+%%
+
 
 save('F.mat','-v7.3','Fcell','FcellNeu','ops','sp','stat');
